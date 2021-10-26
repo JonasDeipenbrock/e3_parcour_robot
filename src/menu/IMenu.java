5a75d17bf51577c1ebceb5b78c0eed791c6a2e78
Jonas Deipenbrock <jonas.deipenbrock@gmx.de>
2021-10-26 11:13:41 +0200

    add basic menu
    adds a basic textmenu
    adds a basic interface to wrap common functions
    untested
    
    Signed-off-by: Jonas Deipenbrock <jonas.deipenbrock@gmx.de>

---
+++ src/menu/IMenu.java
@@ -0,0 +1,7 @@
+package menu;
+
+public interface IMenu {
+	public void drawMenu(String[] items);
+	public int selectMenuElement();
+	public void clearMenu();
+}
---
+++ src/menu/StartMenu.java
@@ -0,0 +1,38 @@
+package menu;
+
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.lcd.GraphicsLCD;
+import lejos.utility.TextMenu;
+
+public class StartMenu implements IMenu {
+	
+	public GraphicsLCD display;
+	public TextMenu menu;
+	public StartMenu() {
+		display = LocalEV3.get().getGraphicsLCD();
+	}
+
+	@Override
+	public void drawMenu(String[] items) {
+		menu = new TextMenu(items, 1, "Choose a mode");
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int selectMenuElement() {
+		// TODO Auto-generated method stub
+		if(menu == null) {
+			return -1;
+		}
+		int selection = menu.select();
+		return selection;
+	}
+	
+	@Override
+	public void clearMenu() {
+		display.clear();
+		menu = null;
+	}
+
+}
