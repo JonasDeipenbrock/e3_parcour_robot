79dc8c22590ab4829fc2157394d0afff8a489a32
Jonas Deipenbrock <jonas.deipenbrock@gmx.de>
2021-11-15 11:54:06 +0100

    move init to calibration and menu loop
    Signed-off-by: Jonas Deipenbrock <jonas.deipenbrock@gmx.de>

--- src/e3base/ColorSensor.java
+++ src/e3base/ColorSensor.java
@@ -8,12 +8,19 @@ public class ColorSensor {
     static private ColorSensor singleton;
     private EV3ColorSensor colorSensor;
     private SampleProvider colorSampleProvider;
-    private float[] data = new float[4];
+    private float[] data = new float[3];
+    
+    public float whiteValue = 0.2f;
+    public float blackValue = 0.01f;
+    public float getBorderValue() {
+    	return (whiteValue + blackValue) / 2;
+    }
 
     private ColorSensor() {
     	colorSensor = new EV3ColorSensor(Configuration.colorSensorPort);
     	colorSensor.setCurrentMode(2);
     	colorSampleProvider = colorSensor.getRGBMode();
+    	colorSampleProvider.fetchSample(data, 0);
     }
     
     public void setMode(int modeID) {
@@ -27,12 +34,15 @@ public class ColorSensor {
 
         return singleton;
     }
+    
+    public float getGreyScale() {
+    	colorSampleProvider.fetchSample(data, 0);
+    	float grey = (data[0] + data[1] + data[2]) / 3;
+    	return grey;
+    }
 
     public float[] getColorData() {
     	colorSampleProvider.fetchSample(data, 0);
-    	//colorSensor.fetchSample(data, 0);
-        //ultrasonicSampleProvider.fetchSample(data, 0);
-    	//System.out.println(data);
         return data;
     }
 
