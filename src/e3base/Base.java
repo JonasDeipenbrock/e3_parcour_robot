8b1903eb79f6f4d9fec0d414dee9d814485ec44d
Jonas Deipenbrock <jonas.deipenbrock@gmx.de>
2021-11-16 15:48:38 +0100

    timeout and bugfixes
    fixes main loop exit breaking
    fixes movement with negativ degrees
    adds timeout via driven angle
    
    Signed-off-by: Jonas Deipenbrock <jonas.deipenbrock@gmx.de>

--- src/e3base/Base.java
+++ src/e3base/Base.java
@@ -1,6 +1,6 @@
 package e3base;
 
-import lejos.hardware.Button;
+import lejos.hardware.lcd.LCD;
 import lejos.hardware.sensor.EV3TouchSensor;
 import lejos.utility.Delay;
 import levelSolver.Algorithms;
@@ -13,29 +13,22 @@ public class Base {
 	public static EV3TouchSensor rightTouch = new EV3TouchSensor(Configuration.rightTouchSensorPort);
 
 	public static void main(String[] args) {
-		
-//		float[] sensorDataLeft = new float[1];
-//		float[] sensorDataRight = new float[2];
-		//Movement move = Movement.getInstance();
-		 
-		//Move the pilot to its own class and asbtract its movement methods for our uses in the algorithms
-//		DifferentialPilot robot = new DifferentialPilot(
-//				Configuration.wheelDiameter, 
-//				Configuration.trackWidth, 
-//				left, 
-//				right, 
-//				true);
-//		robot.setAcceleration(4000);
-////		robot.setTravelSpeed(20);
-//		robot.setRotateSpeed(180);
-		
-		while (Button.ESCAPE.isUp()) {
+		runLoop();
+		endProgram();
+	}
+	
+	public static void runLoop() {
+		while (true) {
+			LCD.clearDisplay();
 			// Draws the basic menu and wait for the user to select a value
 			String[] items = Algorithms.getNames();
 			StartMenu startMenu = new StartMenu();
 			Delay.msDelay(1000);
 			startMenu.drawMenu(items);
 			int selectedMode = startMenu.selectMenuElement();
+			if(selectedMode == -1) {
+				break;
+			}
 			startMenu.clearMenu();
 			Algorithms value = Algorithms.values()[selectedMode];
 			try {
@@ -45,21 +38,16 @@ public class Base {
 				e.printStackTrace();
 				System.exit(1);
 			}
-		
-			// -> All algorithms should run until the user interrupts via button press
 		}
-		
+	}
+	
+	public static void endProgram() {
 		Movement.close();
 		UltrasonicPosition.close();
 		UltrasonicSensor.close();
 		leftTouch.close();
 		rightTouch.close();
-		//colorSensor.close();
 		ColorSensor.close();
-	}
-	
-	public static void runLoop() {
-		
 	}
 
 }
