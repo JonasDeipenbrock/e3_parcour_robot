e1346bf2122f63e2ab16adec50838e39d2e4b41a
sepro <4618135+seproDev@users.noreply.github.com>
2022-01-10 11:43:01 +0100

    Added proper exit handling to bridge crossing

--- src/levelSolver/BridgeCrossing.java
+++ src/levelSolver/BridgeCrossing.java
@@ -1,7 +1,6 @@
 package levelSolver;
 
-import drivingConditions.ComparisonMethod;
-import drivingConditions.UltrasonicCondition;
+import drivingConditions.*;
 import lejos.hardware.Button;
 import wrappers.ExitCode;
 import wrappers.Movement;
@@ -10,7 +9,7 @@ import wrappers.UltrasonicSensor;
 public class BridgeCrossing implements ILevelSolver {
 	
 	private Movement movement;
-	UltrasonicSensor usSensor;
+	private UltrasonicSensor usSensor;
 	
 	public BridgeCrossing() {
 		movement = Movement.getInstance();
@@ -19,27 +18,73 @@ public class BridgeCrossing implements ILevelSolver {
 
 	@Override
 	public ExitCode run() {
-			System.out.println("Starting bridge crossing");
+		System.out.println("Starting bridge crossing");
 
-			//drive right curve and correct when ip sensor sees ravine
-			movement.moveByDistance(15);
-			while (Button.ENTER.isUp()) {
-				movement.setMotorRotation(50, 300);
-				movement.forward();
-				movement.waitUntil(new UltrasonicCondition(ComparisonMethod.GREATER, 0.07f));
-				movement.stop();
-				System.out.println(usSensor.getDistance());
-				if (usSensor.getDistance() >= 1000) {
-					System.out.println("Infinity");
-					break;
-				}
-				movement.moveByDistance(-5);
-				movement.setSpeed(400f);
-				movement.turnLeft();
-				movement.waitUntil(new UltrasonicCondition(ComparisonMethod.LESS, 0.06f));
-				movement.stop();
-				movement.turn(30);
+		IDrivingCondition forwardCond = new OrCondition(
+				new UltrasonicCondition(ComparisonMethod.GREATER, 0.07f),
+				new BlueStripCondition(),
+				new BumperCondition(),
+				new ButtonCondition());
+
+		IDrivingCondition turnCond = new OrCondition(
+				new UltrasonicCondition(ComparisonMethod.LESS, 0.06f),
+				new BlueStripCondition(),
+				new BumperCondition(),
+				new ButtonCondition());
+
+		movement.moveByDistance(15);
+		while (true) {
+			//drive forward in slight arc
+			movement.setMotorRotation(50, 300);
+			movement.forward();
+			int status = movement.waitUntil(forwardCond);
+			movement.stop();
+
+			if (status == 2) {
+				System.out.println("Detected Blue Strip");
+				return ExitCode.SUCCESSFULL;
+			} else if (status == 3) {
+				System.out.println("Bumper pushed. Starting correction code");
+				bumperNavigate();
+				return ExitCode.SUCCESSFULL;
+			} else if (status == 4) {
+				System.out.println("Button pushed. Exiting");
+				return ExitCode.USER_INTERRUPT;
 			}
-			return ExitCode.SUCCESSFULL;
+
+
+			if (usSensor.getDistance() >= 1000) {
+				System.out.println("Infinity");
+				//return ExitCode.SUCCESSFULL;
+			}
+
+			//drive back and turn left
+			movement.moveByDistance(-5);
+			movement.setSpeed(400f);
+			movement.turnLeft();
+			status = movement.waitUntil(turnCond);
+			if (status == 2) {
+				System.out.println("Detected Blue Strip");
+				return ExitCode.SUCCESSFULL;
+			} else if (status == 3) {
+				System.out.println("Bumper pushed. Starting correction code");
+				bumperNavigate();
+				return ExitCode.SUCCESSFULL;
+			} else if (status == 4) {
+				System.out.println("Button pushed. Exiting");
+				return ExitCode.USER_INTERRUPT;
+			}
+
+			movement.stop();
+			movement.turn(30);
+		}
+	}
+
+	private void handleStatus() {
+
+	}
+
+	private void bumperNavigate() {
+		//TODO: find entrance code
 	}
 }
