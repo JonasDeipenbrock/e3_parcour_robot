bfcf98e75965a31252a224938be926920396de6a
sepro <4618135+seproDev@users.noreply.github.com>
2022-01-17 10:04:38 +0100

    reformat code

--- src/levelSolver/FindAndPush.java
+++ src/levelSolver/FindAndPush.java
@@ -26,43 +26,43 @@ public class FindAndPush implements ILevelSolver {
 		ColorSensor colorSensor = ColorSensor.getInstance();
 		Helper helper = Helper.getInstance();
 		StatusIndicator ind = StatusIndicator.getInstance();
-		UltrasonicPosition uPosition = UltrasonicPosition.getInstance()
-		
+		UltrasonicPosition uPosition = UltrasonicPosition.getInstance();
+
 		uPosition.moveUP();
 
 		ind.setLED(LEDPattern.LED_RED);
-		
+
 		movement.turnLeft90();
 		movement.forwardUntil(new OrCondition(new BumperCondition(),
-											  new ButtonCondition()));
+				                              new ButtonCondition()));
 		movement.moveByDistance(1);
 		movement.moveByDistance(-10);
 		movement.turnRight90();
 		movement.setToMaxSpeed();
 		movement.forwardUntil(new OrCondition(new UltrasonicCondition(ComparisonMethod.GREATER, 0.4f),
-											  new TimeoutCondition(2000)));
+		                                      new TimeoutCondition(2000)));
 		ind.setLED(LEDPattern.LED_ORANGE);
-		
+
 		System.out.println("Moving to box");
 		movement.forwardUntil(new OrCondition(new UltrasonicCondition(ComparisonMethod.LESS, 0.4f),
-											  new ButtonCondition()));
-		
-        ind.setLED(LEDPattern.LED_ORANGE);
- 		//move a bit further -> in front of box
+				                              new ButtonCondition()));
+
+		ind.setLED(LEDPattern.LED_ORANGE);
+		// move a bit further -> in front of box
 		movement.moveByDistance(10);
 		movement.turnRight90();
 		System.out.println("Pushing the box");
-		
+
 		ind.setLED(LEDPattern.LED_GREEN_FLASH);
 		// Move forward until Motors stop or push sensor -> Push box against wall
 		movement.forwardUntil(new OrCondition(new TimeoutCondition(4000),
-										      new LinesPassedCondition(2),
-											  new ButtonCondition()));
+				                              new LinesPassedCondition(2),
+				                              new ButtonCondition()));
 		movement.moveByDistance(15);
 		System.out.println("At wall");
 
 		ind.setLED(LEDPattern.LED_ORANGE_FLASH);
-		//Move around box by one side
+		// Move around box by one side
 		movement.moveByDistance(-2);
 		movement.turnRight90();
 		movement.moveByDistance(20);
@@ -70,34 +70,35 @@ public class FindAndPush implements ILevelSolver {
 		movement.moveByDistance(15);
 		movement.turnLeft90();
 
-		//Move box in corner
+		// Move box in corner
 		movement.forwardUntil(new OrCondition(new TimeoutCondition(3000),
-											  new LinesPassedCondition(1),
-											  new ButtonCondition()));
+				                              new LinesPassedCondition(1),
+				                              new ButtonCondition()));
 
 		ind.setLED(LEDPattern.LED_RED_FLASH);
-		//Drive to exit
+		// Drive to exit
 		movement.moveByDistance(-10);
 		movement.turnLeft90();
 
-		//shortly move backwards to ensure alignment
+		// shortly move backwards to ensure alignment
 		movement.backward();
 		Delay.msDelay(100);
 		movement.stop();
 
 		movement.forwardUntil(new OrCondition(new LinesPassedCondition(1),
-											  new ButtonCondition()));
+				                              new ButtonCondition()));
 
 		movement.turnLeft90();
 		ind.setLED(LEDPattern.LED_GREEN);
 
-		//ausrichtung an wand -> 50cm ideal
+		// ausrichtung an wand -> 50cm ideal
 		movement.forward();
-		while (!colorSensor.checkBlue()) {	//stop on blue line
-			if (!helper.checkLoop(false, false)) break;
+		while (!colorSensor.checkBlue()) { // stop on blue line
+			if (!helper.checkLoop(false, false))
+				break;
 			float distanceRight = uSensor.getDistance();
-			//we want to reach 0.5m to the right ideally
-			float offset = (0.4f - distanceRight) * -500;	//error is difference from ideal wall point
+			// we want to reach 0.5m to the right ideally
+			float offset = (0.4f - distanceRight) * -500; // error is difference from ideal wall point
 			movement.setMotorRotation(offset, 500f);
 			movement.forward();
 		}
