c1926c4069d9188a55cf4ca4020a45ae7a2becaf
sepro <4618135+seproDev@users.noreply.github.com>
2022-01-24 12:38:11 +0100

    Added TachoCondition

---
+++ src/drivingConditions/TachoCondition.java
@@ -0,0 +1,23 @@
+package drivingConditions;
+
+import wrappers.Movement;
+
+public class TachoCondition implements IDrivingCondition {
+
+    private int tachoDifference;
+    private Movement move;
+	private int[] initial;
+
+    public TachoCondition(int tachoDifference) {
+        this.tachoDifference = tachoDifference;
+        this.move = Movement.getInstance();
+        this.initial = move.getTachoCount();
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        int[] currentVal = move.getTachoCount();
+        int tachoDiff = Math.abs((initial[0] - currentVal[0]) - (initial[1] - currentVal[2]));
+        return tachoDiff >= tachoDifference ? 1 : 0;
+    }
+}
