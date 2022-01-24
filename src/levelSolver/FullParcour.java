bea98232e59588a0f8b48bc31696490a0a00cd91
sepro <4618135+seproDev@users.noreply.github.com>
2022-01-24 09:45:26 +0100

    Added proper full parcour

--- src/levelSolver/FullParcour.java
+++ src/levelSolver/FullParcour.java
@@ -1,20 +1,15 @@
 package levelSolver;
 
-import drivingConditions.OrCondition;
-import drivingConditions.BumperCondition;
-import drivingConditions.TimeoutCondition;
 import wrappers.ExitCode;
-import wrappers.Movement;
 
 public class FullParcour implements ILevelSolver {
 
 	@Override
 	public ExitCode run() {
-		// TODO Auto-generated method stub
-		Movement move = Movement.getInstance();
-		move.setSpeed(500);
-		move.setToMaxAcc();
-		move.forwardUntil(new OrCondition(new TimeoutCondition(5000), new BumperCondition()));
+		new LineFollowingV2().run();
+		new FindAndPush().run();
+		new BridgeCrossing().run();
+		new FindCrosses().run();
 		return ExitCode.SUCCESSFULL;
 	}
 }
